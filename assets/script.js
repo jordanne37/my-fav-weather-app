4fa64aaa3c42c4be3ab6d40b0945b72b98d31ab6
JordAnne <jbethune37@gmail.com>
2023-03-02 21:06:26 -0500

    added DOM elements to js. updated the html

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+var inputEl = document.querySelector(".input");
+var searchBtnEl = document.querySelector(".search-btn");
+var cityEL = document.querySelector(".city")
+var tempEl = document.querySelector(".temperature");
+var windEl = document.querySelector(".wind");
+var humidityEl = document.querySelector(".humidity");
+var weathericonEl = document.querySelector(".weather-icon");
+var futureEl = document.querySelector(".future");
+var formEl = document.querySelector(".city-form");
+var dateEl = document.querySelector(".date");
+
