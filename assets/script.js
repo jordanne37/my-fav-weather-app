a81a5de97c4b99e0b8a68ca02d41647d8827030c
JordAnne <jbethune37@gmail.com>
2023-03-05 14:35:07 -0500

    fixed errors to get the temp, wind, and humidity to show

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+// DOM Elements
 var inputEl = document.querySelector(".input");
 var searchBtnEl = document.querySelector(".search-btn");
 var cityEL = document.querySelector(".city")
@@ -7,7 +8,7 @@ var humidityEl = document.querySelector(".humidity");
 var weathericonEl = document.querySelector(".weather-icon");
 var futureEl = document.querySelector(".future");
 var formEl = document.querySelector(".city-form");
-var dateEl = document.querySelector(".date");
+var dateEl = document.querySelector(".date")
 
 var cityWeather = function(event) {
     event.preventDefault();
@@ -24,30 +25,34 @@ var cityWeather = function(event) {
 formEl.addEventListener("submit", cityWeather)
 function currentWeather(weatherQuery) {
     var geoApiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + weatherQuery+ "&appid=9f1c292ce1ea1de6d33b5afbe52ac1d9"
+    
+
     fetch(geoApiUrl)
     .then(response=> response.json())
     .then(data => {
+    
+        console.log(data)
+        tempEl.textContent = data.main.temp + " degrees"
+        windEl.textContent = data.wind.speed + " mp/h"
+        cityEL.textContent = data.name
+        humidityEl.textContent = data.main.humidity + "%"
+        weathericonEl.src = 'https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png'
+        // dateEl.textContent = theCurrentDate
+        // weathericonEl.src = 'https://openweathermap.org/img/w/'
+        // humidityEl.textContent = data.main.humidity + "%"
+
 
         const futureApiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${data.coord.lat}&lon=${data.coord.lon}&appid=8ec9bee709b02f3b8fc2ad8d9351b0e0&units=imperial`
                  return fetch(futureApiUrl);
-                })
-        .then(response => response.json())
-        .then(data => {
         
-            var currentDate = data.coord.dt;
-            var theCurrentDate = dayjs(JSON.stringify(currentDate)).format("MMMM D, YYYY")
-        
-        console.log(data)
-        tempEl.textContent = data.main.temp
-        windEl.textContent = data.wind.speed + " mp/h"
-        cityEL.textContent = data.name
-        dateEl.textContent = theCurrentDate
-        weathericonEl.src = 'https://openweathermap.org/img/wn/'
-        humidityEl.textContent = data.main.humidity + "%"
-
+                 
+       
     })
-    //console.log(data)
-
+    .then(response => response.json())
+    .then(data => {
+    console.log(data)
+   
+    })
     
 }
 
@@ -56,50 +61,49 @@ function futureWeather(weatherQuery) {
     fetch(geoApiUrl)
     .then(response=> response.json())
     .then(data => {
-    const weatherApi = `https://api.openweathermap.org/data/2.5/forecast?lat=${data.coord.lat}&lon=${data.coord.lon}&appid=912d45dc6bbe2ccd1b94d91571732ef8&units=imperial`
-    return fetch(weatherApi);
+        const weatherApi = `https://api.openweathermap.org/data/2.5/forecast?lat=${data.coord.lat}&lon=${data.coord.lon}&appid=912d45dc6bbe2ccd1b94d91571732ef8&units=imperial`
+        return fetch(weatherApi);
     })
     .then(response => response.json())
     .then(data => {
-        console.log(data)
-        var futWeather = "";
-        for(var i = 0; i <= data.list.length; i++) {
-            var date = data.list[i].dt_txt.substr(11,8);
-            var futDate = data.list[i].dt_txt.substr(0,11);
-            var theFutureDate =dayjs(JSON.stringify(futDate)).format("MMMM D, YYYY");
-            if(date === "12:00:00") {
+        console.log(data);
+        var futWeather ="";
+        console.log(futWeather)
+        for(var i =0; i <= data.list.length; i++) {
+            var theDate = data.list[i].dt_txt.substr(11,8);
+            console.log("New Date" + theDate);
+            var futureDate = data.list[i].dt_txt.substr(0,11);
+            console.log("futureDate" + futureDate)
+            var updatedDate = dayjs(JSON.stringify(futureDate)).format("MMMM D, YYYY");
+            if(theDate === "12:00:00") {
 
-            futWeather += `
-           
-           <div class= "card flex-column future">
-                <div class="text-xl">${theFutureDate}</div>
-                <img class="mt-5 text-5xl self-center inline-flex"
-                src="https://openweathermap.org/img/wn/${data.list[i].weather[0].icon}@2x.png">
-                
-                <div class="flex flex-row items-center justify-center mt-6">
-                <div class="font-large text-5xl">${data.list[i].main.temp}°F</div>
-                <div class="flex flex-col items-center ml-6">
-                    <div>${data.list[i].weather[0].description}</div>
-                </div>
-            </div>
-            <div class="flex-row justify-between mt-6">
-                <div class="flex flex-col items-center">
-                <div class="font-medium text-sm">Wind</div>
-                <div class="text-sm text-black">${data.list[i].wind.speed}mp/h</div>
-            </div>
-            <div class="flex-col items-center">
-                <div class="font-medium text-sm">Humidity</div>
-                <div class="text-sm text-black">${data.list[i].main.humidity}%</div>
-                </div>
-                </div>
-            </div>
+                futWeather += `
+            <div class="flex flex-col bg-white rounded p-4 w-full max-w-xs dashboard-future">
+                                    <div class="font-bold text-xl">${updatedDate}</div>
+                                    <img class="mt-6 text-6xl self-center inline-flex items-center justify-center rounded-lg h-24 w-24"
+                                       src="http://openweathermap.org/img/wn/${data.list[i].weather[0].icon}@2x.png" alt="Weather Icon"
+                                    >
+                                    <div class="flex flex-row items-center justify-center mt-6">
+                                        <div class="font-large text-5xl">${data.list[i].main.temp}°F</div>
+                                        <div class="flex flex-col items-center ml-6">
+                                            <div>${data.list[i].weather[0].description}</div>
+                                        </div>
+                                    </div>
+                                    <div class="flex-row justify-between mt-6">
+                                        <div class="flex flex-col items-center">
+                                            <div class="font-medium text-sm">Wind</div>
+                                            <div class="text-sm text-black">${data.list[i].wind.speed}mp/h</div>
+                                        </div>
+                                        <div class="flex-col items-center">
+                                            <div class="font-medium text-sm">Humidity</div>
+                                            <div class="text-sm text-black">${data.list[i].main.humidity}%</div>
+                                        </div>
+                                    </div>
+                                </div>
+            `
+            futureEl.innerHTML = futWeather;
+           }
 
-
-`
-futureEl.innerHTML = futWeather;
-
-
-            }
-        }
+       }
     })
 }
