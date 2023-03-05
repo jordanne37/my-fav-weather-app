8defdaea54b3190e6852dff6780c5c5b80a52abf
JordAnne <jbethune37@gmail.com>
2023-03-05 12:39:43 -0500

    added innerhtml to js. still need errors fixed

--- assets/script.js
+++ assets/script.js
@@ -27,27 +27,28 @@ function currentWeather(weatherQuery) {
     fetch(geoApiUrl)
     .then(response=> response.json())
     .then(data => {
+
+        const futureApiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${data.coord.lat}&lon=${data.coord.lon}&appid=8ec9bee709b02f3b8fc2ad8d9351b0e0&units=imperial`
+                 return fetch(futureApiUrl);
+                })
+        .then(response => response.json())
+        .then(data => {
         
+            var theCurrentDate = data.coord.dt;
+            var theCurrentDate = dayjs(JSON.stringify(currentDate)).format("MMMM D, YYYY")
         
         console.log(data)
         tempEl.textContent = data.main.temp
         windEl.textContent = data.wind.speed + " mp/h"
         cityEL.textContent = data.name
         dateEl.textContent = theCurrentDate
-        weathericonEl.src = 'https://openweathermap.org/img/w/'
+        weathericonEl.src = 'https://openweathermap.org/img/wn/'
         humidityEl.textContent = data.main.humidity + "%"
 
-
-        const futureApiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${data.coord.lat}&lon=${data.coord.lon}&appid=8ec9bee709b02f3b8fc2ad8d9351b0e0&units=imperial`
-                 return fetch(futureApiUrl);
     })
-    .then(response => response.json())
-    .then(data => {
     //console.log(data)
 
-    var currentDate = data.coord.dt;
-    // var theCurrentDate = dayjs(JSON.stringify(currentDate)).format("MMMM D, YYYY")
-    })
+    
 }
 
 function futureWeather(weatherQuery) {
@@ -56,10 +57,32 @@ function futureWeather(weatherQuery) {
     .then(response=> response.json())
     .then(data => {
     const weatherApi = `https://api.openweathermap.org/data/2.5/forecast?lat=${data.coord.lat}&lon=${data.coord.lon}&appid=912d45dc6bbe2ccd1b94d91571732ef8&units=imperial`
-        return fetch(weatherApi);
+    return fetch(weatherApi);
     })
     .then(response => response.json())
-    
+    .then(data => {
+        console.log(data)
+        var futWeather = "";
+        for(var i = 0; i <= data.list.length; i++) {
+            var date = data.list[i].dt_txt.substr(11,8);
+            var futDate = data.list[i].dt_txt.substr(0,11);
+            var theFutureDate =dayjs(JSON.stringify(futDate)).format("MMMM D, YYYY");
+            if(date === "12:00:00") {
+
+            futWeather +=
+           
+           <div class= "card flex-column future">
+                <div class="text-xl">${theFutureDate}</div>
+                <img class="mt-5 text-5xl self-center inline-flex"
+                src="https://openweathermap.org/img/wn/${data.list[i].weather[0].icon}@2x.png">
+
+                </img>
+                
+            </div>
+futureEl.innerHTML = futWeather;
 
 
+            }
+        }
+    })
 }
